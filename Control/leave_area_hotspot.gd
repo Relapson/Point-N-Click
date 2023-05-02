01a3d7d118e806f94da811bc4ed7231137f9b805
Ferdinand Mentzen <ferdimentzen@gmx.de>
2023-05-02 14:16:36 +0200

    zwischencommit, bevor objekte local to scene

--- Control/leave_area_hotspot.gd
+++ Control/leave_area_hotspot.gd
@@ -5,6 +5,7 @@ extends Area2D
 @export var next_room: PackedScene
 @export var room_name: String = ""
 var showing = false
+# area left variable einführen, damit spieler durch die selbe stelle den bereich verlassen/betreten kann
 
 func _ready():
 	$RoomName.set_text(next_room.resource_path)
@@ -21,6 +22,7 @@ func _on_input_event(_viewport, event, _shape_idx):
 		if event.double_click:
 			print("INSTANT") # switch to scene here
 
+# zusätzlich prüfen ob bereich geklickt wurde
 func _on_player_body_entered(body):
 	print(body)
 	# switch the scene here
