260c62a025f4a90fe0121461519d678c6a8e2def
Ferdinand Mentzen <ferdimentzen@gmx.de>
2023-05-03 14:22:32 +0200

    hotspots mit signalen versehen

--- Control/leave_area_hotspot.gd
+++ Control/leave_area_hotspot.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
 @export_file var next_room # ERROR: nicht die packed scene, sondern den pfad dazu referenzieren
+@export var exit_position: Vector2
 #@export var room_name: String = "" optional vielleicht
 var showing = false
-var clicked_on = false
+var body_in_area = false
 # area left variable einführen, damit spieler durch die selbe stelle den bereich verlassen/betreten kann
 
 func _ready():
@@ -13,18 +14,21 @@ func _ready():
 func _on_input_event(_viewport, event, _shape_idx):
 	# check for doubleclick for instant scene transition
 	if event is InputEventMouseButton:
-		if event.double_click:
-			clicked_on = false
+		if event.get_button_index() == 1 and body_in_area:
+			body_in_area = false
 			RoomLoader.goto_scene(next_room)
 			return
-		clicked_on = true
+		if event.double_click:
+			RoomLoader.goto_scene(next_room)
+			return
 
 # zusätzlich prüfen ob bereich geklickt wurde
 func _on_player_body_entered(body):
-	if clicked_on:
-		clicked_on = false
-		RoomLoader.goto_scene(next_room)
-		return
-		
-func set_clicked_on(click):
-	clicked_on = click
+	if body.name == "Player":
+		body_in_area = true
+		print(body_in_area)
+
+func _on_player_body_exited(body):
+	if body.name == "Player":
+		body_in_area = false
+		print(body_in_area)
