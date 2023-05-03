8b83c53b7525d291a398ae7f337c3a64e420de09
Ferdinand Mentzen <ferdimentzen@gmx.de>
2023-05-03 18:54:52 +0200

    leave_area_hotspots done (vorerst)

--- Control/leave_area_hotspot.gd
+++ Control/leave_area_hotspot.gd
@@ -1,45 +1,36 @@
 extends Area2D
 
-@export_file var next_room # ERROR: nicht die packed scene, sondern den pfad dazu referenzieren
-@export var exit_position: Vector2
-#@export var room_name: String = "" optional vielleicht
-var showing = false
+@export_file var next_room # pfad zur scene
+@export var exit_position: Vector2 # stelle zum wiedereintritt
 var body_in_area = false
-var clicked_area = false
-# area left variable einführen, damit spieler durch die selbe stelle den bereich verlassen/betreten kann
+var clicked = false # TODO: geht vielleicht besser, funktioniert aber
 
 func _ready():
 	$RoomName.set_text(next_room if next_room != null else "")
 	$RoomName.hide()
 
-# wenn nicht im bereich geklickt wurde -> false
-#func _input(event):
-#	if event is InputEventMouseButton:
-#		if event.get_button_index() == 1:
-#			clicked_area = false
+func _process(delta):
+	if clicked and body_in_area:
+		await get_tree().create_timer(0.2).timeout # TODO: kurz warten bevor szene gewechselt wird - evtl auch animation abspielen/abwarten dann
+		RoomLoader.goto_scene(next_room)
+		return
 
 # wenn im bereich geklickt wurde -> true
 func _on_input_event(_viewport, event, _shape_idx):
-	# check for doubleclick for instant scene transition
 	if event is InputEventMouseButton:
+		clicked = true
+		# check for doubleclick for instant scene transition
 		if event.double_click:
+			clicked = false
 			RoomLoader.goto_scene(next_room)
 			return
-		if event.get_button_index() == 1:
-			clicked_area = true
-		if (event.get_button_index() == 1 and body_in_area) or clicked_area:
-			body_in_area = false
-			clicked_area = false
-			RoomLoader.goto_scene(next_room)
-			return
+		if event.get_button_index() == 1 and body_in_area:
+			clicked = true
 
-# zusätzlich prüfen ob bereich geklickt wurde
 func _on_player_body_entered(body):
 	if body.name == "Player":
 		body_in_area = true
-		print(body_in_area)
 
 func _on_player_body_exited(body):
 	if body.name == "Player":
 		body_in_area = false
-		print(body_in_area)
