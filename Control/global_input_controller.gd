77c7b01e6347f057e17eea4a915e6e751c945b96
Ferdinand Mentzen <ferdimentzen@gmx.de>
2023-06-01 03:14:57 +0200

    erstes inventar overlay sichtbar, sonst bisschen rumgepfuscht

---
+++ Control/global_input_controller.gd
@@ -0,0 +1,23 @@
+extends Node
+
+@onready var inventory_gui = load("res://Control/itemControl/inventory_gui.tscn")
+
+var inventory_open = false
+
+func _input(event):
+	pass
+#	print(event)
+#	if event is InputEventKey and event.keycode == KEY_I:
+#		open_and_close_inventory()
+#
+#func _process(_delta):
+#	if Input.is_action_just_pressed("open_inv"):
+#		open_and_close_inventory()
+		
+func open_and_close_inventory():
+	pass
+#	if !inventory_open:
+#		get_tree().root.add_child(inventory_gui.instantiate())
+#	else:
+#		get_tree().root.remove_child(find_child("inventory_gui"))
+#	inventory_open = !inventory_open
--- Control/itemControl/inventory_gui.gd
+++ Control/itemControl/inventory_gui.gd
@@ -1,5 +1,17 @@
 extends Node
 
+func _input(_event):
+	if Input.is_action_just_pressed("open_inv"):
+#	if event is InputEventKey and event.keycode == KEY_I:
+		$CenterContainer.visible = !$CenterContainer.visible
+
 func _ready():
-	for item in GlobalInventory.picked_up_items:
-		$Control/ItemList.add_item(item)
+	$CenterContainer.visible = false
+	
+func update_inventory():
+	# updated every position in the inventory
+	pass
+
+func update_inventory_at_position(position:int):
+	# updates inventory at specific position
+	pass
