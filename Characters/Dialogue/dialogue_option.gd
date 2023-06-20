3012421f80fdc662b62c0bfc166b22ad6daf20f7
Ferdinand Mentzen <ferdimentzen@gmx.de>
2023-06-20 15:10:46 +0200

    andere dialogoptionen, npc bild wird neben text angezeigt, anpassungen im manager

--- Characters/Dialogue/dialogue_option.gd
+++ Characters/Dialogue/dialogue_option.gd
@@ -8,7 +8,7 @@ func _ready():
 	$DialogueOption.add_theme_color_override("font_hover_color", Color(0,1,0))
 
 func _on_dialogue_option_pressed():
-	print("PRESSED")
+	print("PRESSED: " + $DialogueOption.text)
 	if end_function:
 		print("END DIALOGUE")
 		end_dialogue.emit()
--- Characters/Dialogue/dialogue_scene_manager.gd
+++ Characters/Dialogue/dialogue_scene_manager.gd
@@ -1,5 +1,9 @@
 extends Control
 
+var dialogue_participants = {
+	"player": load("res://Assets/Dialogue Icons/icon_player.svg")
+}
+
 func _ready():
 	# gamevent bus verbinden
 	EventHandler.connect("dialogue_started_event", _start_dialogue)
@@ -8,6 +12,7 @@ func _ready():
 func _start_dialogue(npc_dialogue:Dialogue):
 	EventHandler.emit_signal("set_player_movement", false)
 	_instantiate_dialogue_options(npc_dialogue.dialogue_options)
+	_instantiate_dialogue(npc_dialogue)
 	get_parent().show()
 	
 func _end_dialogue():
@@ -18,17 +23,35 @@ func _end_dialogue():
 func _instantiate_dialogue_options(options:Array):
 	var num = 1
 	for opt in options:
-		var text = opt["text"]
-		var dia_opt = ResourceLoader.load("res://Characters/Dialogue/dialogue_option.tscn")
-		var new_dia_opt = dia_opt.instantiate()
-		new_dia_opt.get_node("DialogueNumber").text = str(num) + ". "
-		new_dia_opt.get_node("DialogueOption").text = text
-		if opt.has("function"):
-			new_dia_opt.end_function = true
-			new_dia_opt.end_dialogue.connect(_end_dialogue)
+		var text_option
+		var dia_opt 
+		var new_dia_opt 
 		
-		$ScrollContainer/AspectRatioContainer/VBoxContainer.add_child(new_dia_opt)
-		num += 1
+		if opt.has("option"):
+			dia_opt = ResourceLoader.load("res://Characters/Dialogue/dialogue_option.tscn")
+			new_dia_opt = dia_opt.instantiate()
+			text_option = opt["option"]
+			new_dia_opt.get_node("DialogueOption").text = text_option
+			new_dia_opt.get_node("DialogueNumber").text = str(num) + ". "
+			$ScrollContainer/AspectRatioContainer/VBoxContainer.add_child(new_dia_opt)
+			num += 1
+			
+		# check if theres an function entry in the JSON
+		# yes -> set the end function on the dialogue option
+		if opt.has("function"):
+			if opt.get("function") == "end_dialogue":
+				new_dia_opt.end_function = true
+				new_dia_opt.end_dialogue.connect(_end_dialogue)
+
+func _instantiate_dialogue(options: Dialogue):
+	# add the npc avatar to the dictionary
+	dialogue_participants["npc"] = options.avatar_texture
+	
+	for opt in options.dialogue_options:
+		if opt.has("welcome_text"):
+			$DialogueOverlay/CharacterName.text = opt.get("display_name") + ":"
+			$DialogueOverlay/Text.text = "\"" + opt.get("welcome_text") + "\""
+			$DialogueOverlay/AvatarImage.texture = dialogue_participants["npc"]
 
 # removes all child nodes from the vbox container if present
 func _reset_dialogue_options():
