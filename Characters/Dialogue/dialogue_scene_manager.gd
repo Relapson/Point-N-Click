2d568863ebfae50159e34481771b014f1b00e7a3
Ferdinand Mentzen <ferdimentzen@gmx.de>
2023-06-30 18:08:57 +0200

    fixes #60, signal-eintrag hat aber noch keine funktionalität

--- Characters/Dialogue/dialogue_scene_manager.gd
+++ Characters/Dialogue/dialogue_scene_manager.gd
@@ -13,7 +13,6 @@ func _ready():
 
 func _start_dialogue(npc_dialogue:Dialogue):
 	EventHandler.emit_signal("set_player_movement", false)
-	_run_dialogue(npc_dialogue.dialogue)
 	_instantiate_dialogue_options(npc_dialogue)
 	get_parent().show()
 	
@@ -23,9 +22,33 @@ func _end_dialogue():
 	get_parent().hide()
 
 # TODO: hier den dialog abspielen
-func _run_dialogue(dialogue:Dictionary):
-	var current_id = dialogue.keys()[0] # an erster stelle immer der dialog start
-	print()
+func _run_dialogue(talk_option:Dictionary, npc_name:String):
+	var timeout = talk_option.get("timer")
+	var player_option = talk_option.get("option_text")
+	var ends_dialogue = talk_option.get("ends_dialogue")
+	var goto_next = talk_option.get("goto_next")
+	
+	# counter to track the array position
+	var npc_counter = 0
+	var player_counter = 0
+	
+	_set_container_visibility(false)
+	
+	# einmal spieler gewählte option abspielen
+	_set_talking(dialogue_participants["player"], "player (placeholder)", player_option)
+	await get_tree().create_timer(timeout).timeout
+	
+	for talking in talk_option.get("is_talking"):
+		_set_talking(dialogue_participants[talking], 
+				npc_name if talking == "npc" else "Player",
+				talk_option.get(talking)[npc_counter] if talking == "npc" else talk_option.get(talking)[player_counter])
+		if talking == "npc": npc_counter += 1
+		if talking == "player": player_counter += 1
+		await get_tree().create_timer(timeout).timeout
+		
+	_set_container_visibility(true)
+	
+	if ends_dialogue: EventHandler.emit_signal("dialogue_ended_event")
 
 # TODO: dialogoptionen hier instanziieren
 func _instantiate_dialogue_options(dia:Dialogue):
@@ -36,26 +59,20 @@ func _instantiate_dialogue_options(dia:Dialogue):
 	var welcome = current_dialogue.welcome
 	var npc_name = current_dialogue.npc_name
 	
+	dialogue_participants["npc"] = dia.avatar_texture
+	
 	for opt in current_dialogue.options:
 		var dia_opt = ResourceLoader.load("res://Characters/Dialogue/dialogue_option.tscn")
 		var new_dia_opt = dia_opt.instantiate() as DialogueOptionContainer
 		
 		new_dia_opt.talk_option = opt
 		new_dia_opt.number = num
+		new_dia_opt.npc_name = npc_name
+		new_dia_opt.selected_option.connect(_run_dialogue)
 		$ScrollContainer/AspectRatioContainer/VBoxContainer.add_child(new_dia_opt)
 		num += 1
 		
-	_set_talking(dia.avatar_texture, npc_name, welcome)
-
-func _instantiate_dialogue(options: Dialogue):
-	# add the npc avatar to the dictionary
-	dialogue_participants["npc"] = options.avatar_texture
-	
-	for opt in options.dialogue_options:
-		if opt.has("welcome_text"):
-			$DialogueOverlay/CharacterName.text = opt.get("display_name") + ":"
-			$DialogueOverlay/Text.text = "\"" + opt.get("welcome_text") + "\""
-			$DialogueOverlay/AvatarImage.texture = dialogue_participants["npc"]
+	_set_talking(dialogue_participants["npc"], npc_name, welcome)
 
 # removes all child nodes from the vbox container if present
 func _reset_dialogue_options():
@@ -63,19 +80,6 @@ func _reset_dialogue_options():
 		return
 	for opt in $ScrollContainer/AspectRatioContainer/VBoxContainer.get_children():
 		opt.queue_free()
-
-func _play_dialogue(text:String, npc_answer:String, player_answer:String, end_function:bool):
-	_set_container_visibility(false) # falls ichs schaffe, das in ne schleife auszulagern nur ein aufruf nötig
-	# TODO: mit schleife über alle möglichen optionen itarieren wär nett
-	_set_talking(dialogue_participants["player"], "Player", text)
-	await get_tree().create_timer(dialogue_timer).timeout
-	_set_talking(dialogue_participants["npc"], "NPC (placeholder)", npc_answer)
-	await get_tree().create_timer(dialogue_timer).timeout
-	_set_talking(dialogue_participants["player"], "Player", player_answer)
-	await get_tree().create_timer(dialogue_timer).timeout
-	_set_container_visibility(true)
-	if end_function:
-		EventHandler.emit_signal("dialogue_ended_event")
 
 func _set_talking(avatar:Texture, talking:String, text:String):
 	$DialogueOverlay/AvatarImage.texture = avatar
